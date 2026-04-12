ef9e62691c23e0d48f96d7f38926b570b3316923
2woghks <ghks8523@naver.com>
2026-04-12 17:02:06 +0900

    합치고 로그인토큰되는지 확인 yaml에서 db이름 확인 나는 pet이고 형은 pet_test임

--- src/main/java/com/example/gamification/repository/MemberRepository.java
+++ src/main/java/com/example/gamification/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package com.example.gamification.repository;
 import com.example.gamification.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
 }
